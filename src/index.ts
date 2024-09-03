169085eadef729e98b34eaf7031af6634be1f5f7
Remi <remi.delaporte@epitech.eu>
2024-09-03 10:31:35 +0200

    feat: zod openapi & swaggerui, biome

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { Hono } from 'hono'
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// The openapi.json will be available at /doc
+app.doc("/doc", {
+	openapi: "3.0.0",
+	info: {
+		version: "1.0.0",
+		title: "My API",
+	},
+});
 
-export default app
+// swagger ui doc will be available at {server url}/ui
+// fell free to change the url
+// swaggerUI url must have same path as openapi.json
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+export default {
+	port: 3000,
+	fetch: app.fetch,
+};
