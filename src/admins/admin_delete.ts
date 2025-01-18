cff20892472c8cb06fa89681989f5f02d2dffba3
Matteo Linzi <matteo.linzi@epitech.eu>
2025-01-18 18:02:53 +0100

    fix: wrong middleware for delete admin route

--- src/admins/admin_delete.ts
+++ src/admins/admin_delete.ts
@@ -1,5 +1,5 @@
 import config from "../config.ts";
-import AdminMiddleware from "../middlewares/middleware_auth.ts";
+import AdminMiddleware from "../middlewares/middleware_admin.ts";
 import { Hono } from "hono";
 
 const admin_delete = new Hono();
