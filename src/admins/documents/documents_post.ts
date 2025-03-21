245a3fef2311517b285898462c00605cfe249a29
Matteo Linzi <matteo.linzi@proton.me>
2025-03-21 16:06:34 +0100

    fix: user uid in admins docs post

--- src/admins/documents/documents_post.ts
+++ src/admins/documents/documents_post.ts
@@ -173,7 +173,7 @@ documents_post.post(
 
 		const increment_total_docs = await config.supabaseClient.rpc(
 			"increment_total_docs",
-			{ p_user_id: user.id, p_docs_to_add: docs.length },
+			{ p_user_id: user.uid, p_docs_to_add: docs.length },
 		);
 		if (increment_total_docs.error != undefined)
 			return c.json({ error: increment_total_docs.error.message }, 500);
